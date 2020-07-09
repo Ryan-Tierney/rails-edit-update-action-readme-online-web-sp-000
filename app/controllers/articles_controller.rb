f23397dddf5b6fee055d71ff2b03baa8809514f1
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-07-09 02:51:52 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,5 +25,8 @@ class ArticlesController < ApplicationController
   end 
   
   def update 
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], discription; params[:article][:description])
+    redirect_to article_path(@article)
   end 
 end
